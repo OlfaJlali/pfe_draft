ac6c7814bcadee75f5354691c3378d660ff72acd
Olfa jlali <olfa.jlali@esprit.tn>
2024-10-19 14:21:25 +0100

    refator: refactor on boarding screen

---
+++ src/styles/OnboardingStyle.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  subtitle: {
+    color: '#282534',
+    fontSize: 13,
+    marginTop: 10,
+    maxWidth: '70%',
+    textAlign: 'center',
+    lineHeight: 23,
+  },
+  title: {
+    color: '#282534',
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginTop: 20,
+    textAlign: 'center',
+  },
+  indicator: {
+    height: 2.5,
+    width: 10,
+    backgroundColor: 'grey',
+    marginHorizontal: 3,
+    borderRadius: 2,
+  },
+  btn: {
+    flex: 1,
+    height: 50,
+    borderRadius: 5,
+    backgroundColor: '#1591ea',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
